26a9bf94c9366790030f126222fe3d1fd83efa2d
smestorage <smestorage@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-18 17:37:20 +0000

    bug fixes for later version
    git-svn-id: https://plugins.svn.wordpress.org/multi-cloud-file-download/trunk@425482 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sme_public_files.php
+++ sme_public_files.php
@@ -1,76 +1,48 @@
 <?php
-
 /*
-
 Plugin Name: SMEStorage Multi-Cloud File Download
-
 Plugin URI: http://www.smestorage.com/
-
 Description: The SMEStorage Multi-Cloud File Download plug-in enables you to share files for download. via a widget,  for over 10 cloud storage providers. These include Amazon S3, RackSpace Cloud Files, MobileMe, Box.net, Microsoft Live SkyDrive, Gmail, any POP3 or IMAP enabled email account, and any FTP server directory.
-
 Author: SME Storage 
-
 Author URI: http://www.smestorage.com/
-
-Version: 1.0.3
-
-
+Version: 1.0.4
 */
 
  
 
 add_option('tag','');
-
 add_option('order','');
-
 add_option('no_of_files','');
-
 add_option('users_names','');
-
 add_option('file_or_time','');
 
+$tag = '';
+$order = '';
+$no_of_files = '';
+$users_names = '';
+$file_or_time = '';
 
-
-
-
-$tag = $_POST['tag'];
-
-$order = $_POST['order'];
-
-$no_of_files = $_POST['no_of_files'];
-
-$users_names = $_POST['users_names'];
-
-$file_or_time = $_POST['file_or_time'];
-
+if(!empty($_POST['tag'])) $tag=$_POST['tag'];
+if(!empty($_POST['order'])) $order = $_POST['order'];
+if(!empty($_POST['no_of_files'])) $no_of_files = $_POST['no_of_files'];
+if(!empty($_POST['users_names'])) $users_names = $_POST['users_names'];
+if(!empty($_POST['file_or_time'])) $file_or_time = $_POST['file_or_time'];
 
 
 if(strlen($tag)>0)
-
 {
-
 	update_option('tag',$tag);
-
 	update_option('order',$order);
-
 	update_option('no_of_files',$no_of_files);
-
-	update_option('file_or_time',$file_or_time);	
-
+	update_option('file_or_time',$file_or_time);
 }
 
 
 
 if(strlen($users_names)>0)
-
 {
-
 	update_option('users_names',$users_names);
-
 }
-
-
-
 
 
 function sme_public_files()
@@ -79,7 +51,7 @@ function sme_public_files()
 	$order = get_option('order');
 	$no_of_files = get_option('no_of_files');
 	$users_names = get_option('users_names');	
-
+	$a=$b=$c=$d='';
 	 switch($order)
 		{		
 			case "last":
@@ -113,25 +85,25 @@ function sme_public_files()
 	<table width="692" border="1">
   <tr>
     <td width="343">Tag</td>
-    <td width="333"><input type="text" value="<? echo $tag;?>" name="tag">&nbsp;(Use * To Show All Files)</td>
+    <td width="333"><input type="text" value="<?php echo $tag;?>" name="tag">&nbsp;(Use * To Show All Files)</td>
   </tr>
   <tr>
     <td>Order</td>
     <td>
 		<select name="order">
 		<option value="">--Select--</option>
-		<option <? echo $a;$a="";?> value="last">Last</option>
-		<option <? echo $b;$b="";?> value="popular">Popular</option>
+		<option <?php echo $a;$a="";?> value="last">Last</option>
+		<option <?php echo $b;$b="";?> value="popular">Popular</option>
 		</select>
 	</td>
   </tr>
   <tr>
     <td align="left">No of Files</td>
-    <td align="left"><input type="text" value="<? if(strlen($no_of_files)>0){ echo $no_of_files;}else {?>20<? }?>" name="no_of_files"></td>
+    <td align="left"><input type="text" value="<?php if(strlen($no_of_files)>0){ echo $no_of_files;}else {?>20<?php }?>" name="no_of_files"></td>
   </tr>
   <tr>
     <td align="left">Option For Displaying FileName or FileName With TimeStamp</td>
-    <td align="left">&nbsp;<input type="radio" name="file_or_time" <? echo $c;?> value="only_file" />&nbsp;Filename&nbsp;<input type="radio" name="file_or_time" <? echo $d;?> value="file_with_time" />&nbsp;Filename With TimeStamp</td>
+    <td align="left">&nbsp;<input type="radio" name="file_or_time" <?php echo $c;?> value="only_file" />&nbsp;Filename&nbsp;<input type="radio" name="file_or_time" <?php echo $d;?> value="file_with_time" />&nbsp;Filename With TimeStamp</td>
   </tr>
   <tr>
     <td>&nbsp;</td>
@@ -145,71 +117,46 @@ function sme_public_files()
 	<table width="250" border="1">
   <tr>
     <td align="left">SME Username</td>
-    <td align="right"><input type="text" name="users_names" value="<? echo $users_names;?>"></td>
+    <td align="right"><input type="text" name="users_names" value="<?php echo $users_names;?>"></td>
   </tr>
   <tr>
     <td>&nbsp;</td>
     <td><input type="submit" value="Update" name="submit"></td>
   </tr>
 </table>	
-	<?
+	<?php
 
 }
 
 function fetch_rss_feed($tag,$order,$no_of_files,$users_names)
 {
 	//$blogurl = "http://www.smetube.com/smestorage/rss/last/xxx/*/20";
-
 	//$blogurl = "http://www.smetube.com/smestorage/rss/".$order."/".$users_names."/".$tag."/".$no_of_files;
 	$blogurl = "http://smestorage.com/rss/".$order."/".$users_names."/".$tag."/".$no_of_files;
 
-
 	$contents = getPageByUrl($blogurl);
-
 	$contents = preg_replace('/<title>/',"******",$contents,2);
-
 	//$contents = preg_replace('/<title>/',"******",$contents,1);
-
 	//$contents = preg_replace('/<link>/',"******",$contents,1);		
-
 	$contents = preg_replace('/<link>/',"******",$contents,2);	
-
 	$contents = preg_replace('/<pubDate>/',"******",$contents,1);
-
 	$total="";
-
 	$file_or_time = get_option('file_or_time');
-
 	while(stripos($contents,'<item>'))
-
-	{	
-
+	{
 		$item = stripos($contents,'<item>',0);	
-
 		$p1 = stripos($contents,'<title>',$item);
-
 		if($p1>0)
-
 		{
-
-			$p2 = stripos($contents,'</title>',$p1);		
-
+			$p2 = stripos($contents,'</title>',$p1);
 			$title = substr($contents,$p1+7,$p2-$p1-7);
-
 					$q1 = stripos($contents,'<link>',$item);
-
 					if($q1>0)
-
 					{
-
 						$q2 = stripos($contents,'</link>',$q1);			
-
 						$link = substr($contents,$q1+6,$q2-$q1-6);	
-
 						//$total .= $link."<br>";
-
 					}
-
 					$r1 = stripos($contents,'<pubDate>',$item);
 
 					if($r1>0)
@@ -254,8 +201,9 @@ function getPageByUrl($url){
 	return $contents;
 }
 
+
 function sme_widget_files() {
-	if ( !function_exists('register_sidebar_widget') )
+	if(!function_exists('register_sidebar_widget') && !function_exists('wp_register_sidebar_widget'))
 		return;
 
 	function sme_widget($args) 
@@ -297,9 +245,16 @@ function sme_widget_files() {
 		echo '<input type="hidden" id="wpaypal-submit" name="sme_submit" value="1" />';
 	}
 
-	register_sidebar_widget('SMEStorage Files', 'sme_widget');
-	register_widget_control('SMEStorage Files', 'control_option', 100, 100);
+	if(function_exists('wp_register_sidebar_widget')){
+		wp_register_sidebar_widget('sme_public_files_w1', 'SMEStorage Files', 'sme_widget', array('description' => __('The SMEStorage Multi-Cloud File Download plug-in enables you to share files for download. via a widget,  for over 10 cloud storage providers. These include Amazon S3, RackSpace Cloud Files, MobileMe, Box.net, Microsoft Live SkyDrive, Gmail, any POP3 or IMAP enabled email account, and any FTP server directory.')));
+		wp_register_widget_control('sme_public_files_w1', 'SMEStorage Files', 'control_option');
+	}elseif(function_exists('register_sidebar_widget')){
+		register_sidebar_widget('SMEStorage Files', 'sme_widget');
+		register_widget_control('SMEStorage Files', 'control_option', 100, 100);
+	}
+
 }
+
 
 add_action('plugins_loaded', 'sme_widget_files');
 add_action('admin_menu', 'sme_tube_public_files');
@@ -310,4 +265,4 @@ function sme_tube_public_files()
 	add_submenu_page(__FILE__, 'SME Files', 'SME Files', 8, __FILE__, 'sme_public_files');
 }
 
- ?>
+?>
